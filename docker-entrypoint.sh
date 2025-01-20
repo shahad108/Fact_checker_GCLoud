136fc251c58e3c5621a5f4173677b13570a5e0ec
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-01-20 17:58:00 -0500

    standardize the docker script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,6 @@ echo "Waiting for database to be ready..."
 python scripts/wait_for_db.py
 
 echo "Running database migrations..."
-# alembic revision --autogenerate -m "Domain credibility made nullable"
-alembic downgrade -1
 alembic upgrade head
 
 echo "Starting the application..."
