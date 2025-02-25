8b3a4b70556c151419f9d54786b5a98e1bcbb32c
taylorlynn <taylor.curtis@mail.mcgill.ca>
2025-02-25 15:10:34 -0500

    fixing embedding generator format

--- app/services/implementations/embedding_generator.py
+++ app/services/implementations/embedding_generator.py
@@ -6,13 +6,14 @@ from sentence_transformers import SentenceTransformer
 logger = logging.getLogger(__name__)
 
 # TODO: Maybe move that to the config file eventually/settings
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
 
 class EmbeddingGenerator(EmbeddingGeneratorInterface):
     def __init__(self):
         self.model = model
 
     async def generate_embedding(self, claim: str) -> List[float]:
-        
+
         embedding = self.model.encode(claim)
         return embedding
